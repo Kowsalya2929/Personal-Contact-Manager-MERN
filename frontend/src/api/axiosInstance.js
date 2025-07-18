06b59cf2d876e6c38b63e093c3f97f80ba850c72
kowsalya L <kowsilogu03@gmail.com>
2025-07-18 10:54:25 +0530

    url changed

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.PORD ? "http://localhost:8000/api" : "https://personal-contact-manager-mern.onrender.com/api"
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: BASE_URL,
     withCredentials: true
 })
 
